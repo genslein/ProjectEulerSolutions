75e32d2d500a104c27b8a063b9ee6eaa64563475
Gabriel Enslein <gabrielenslein@Gabriels-MacBook-Pro.local>
2015-07-02 01:13:24 -0700

    problem 3 add

--- lib/problem3.rb
+++ lib/problem3.rb
@@ -1,11 +1,37 @@
 require_relative 'TimedPut'
-
+require 'Prime'
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
-def largest_prime_factor(number)
-  (number..1).select(0) { |curr, val| val.lcm(number) > curr ? curr = val.lcm(number) : curr}
+def largest_prime_factor_cheat(number)
+    number.prime_division.last[0]
 end
 
-tputs{largest_prime_factor 13195}
-tputs{largest_prime_factor 600851475143}
+def largest_prime_factor_real(number)
+    a = []
+    product_sum = 1
+
+# the digit 2 is the first prime number,
+# so we define it in x
+    x = 2
+
+    while product_sum < number
+      if number % x == 0 && is_prime?(x)
+        a << x
+        product_sum *= x
+      end
+      x += 1
+    end
+    puts a
+end
+
+def is_prime? number
+  (2..(number-1)).each { |x| return false if number % x == 0 }
+  true
+end
+
+tputs{largest_prime_factor_cheat 13195}
+tputs{largest_prime_factor_cheat 600851475143}
+
+tputs{largest_prime_factor_real 13195}
+tputs{largest_prime_factor_real 600851475143}
