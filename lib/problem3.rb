c2576e1613fc723ee40e237b65d9bd86857368b0
Gabriel Enslein <gabrielenslein@Gabriels-MacBook-Pro.local>
2015-07-31 13:07:06 -0700

    alternate problem 3 generation

--- lib/problem3.rb
+++ lib/problem3.rb
@@ -1,5 +1,6 @@
 require_relative 'TimedPut'
 require 'Prime'
+
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
@@ -8,27 +9,39 @@ def largest_prime_factor_cheat(number)
 end
 
 def largest_prime_factor_real(number)
-    a = []
-    product_sum = 1
-
-# the digit 2 is the first prime number,
-# so we define it in x
-    x = 2
-
-    while product_sum < number
-      if number % x == 0 && is_prime?(x)
-        a << x
-        product_sum *= x
+#   a = []
+#   product_sum = 1
+#
+# # the digit 2 is the first prime number,
+# # so we define it in x
+#   x = 2
+#
+#   while product_sum < number
+#     if number % x == 0 && is_prime?(x)
+#       a << x
+#       product_sum *= x
+#     end
+#     x += 1
+#   end
+#   puts a
+    z = 0
+      (2..(Math.sqrt(number-1)).ceil.to_int).each do |y|
+        if number % y == 0 && is_prime?(y) && z < y
+          z = y
+        end
       end
-      x += 1
-    end
-    puts a
+  z
 end
 
 def is_prime? number
-  (2..(number-1)).each { |x| return false if number % x == 0 }
-  true
+  if number % 2 == 0
+    false
+  else
+    (3..(number-1)).step(2) { |x| return false if number % x == 0 }
+    true
+  end
 end
+
 
 tputs{largest_prime_factor_cheat 13195}
 tputs{largest_prime_factor_cheat 600851475143}
--- lib/problem4.rb
+++ lib/problem4.rb
@@ -2,3 +2,6 @@
 # A palindromic number reads the same both ways. The largest palindromic number from 2 two-digit
 # numbers is 91 x 99 = 9009. Find the largest palindrome product from 2 three-digit numbers.
 
+def palindromic_number
+
+end
